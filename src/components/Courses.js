2616b99cdb25fc6c0b46aea2af9c6b173f676a46
Rautenbach89 <105278662+Rautenbach89@users.noreply.github.com>
2023-03-22 07:37:59 +0100

    Update Courses.js

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,16 @@
 import { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
 import axios from "../api/axios";
+import { Link } from "react-router-dom";
 import { LanguageContext } from "../context/LanguageContext";
 import en from "../lang/en.json";
 import de from "../lang/de.json";
-import {
-  faEdit,
-  faTrash
-} from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
+  const [errMsg, setErrMsg] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
   const { language } = useContext(LanguageContext);
   const texts = language === "en" ? en : de;
 
@@ -22,12 +21,11 @@ const Courses = () => {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         });
-        // TODO: remove console.logs before deployment
 
         setCourses(response?.data);
       } catch (err) {
         if (err.response) {
-          // Not in the 200 response range
+
           console.log(err.response.data);
           console.log(err.response.status);
           console.log(err.response.headers);
@@ -39,6 +37,26 @@ const Courses = () => {
 
     fetchCourses();
   }, []);
+
+  const handleSort = (sortBy) => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    setCourses(
+      [...courses].sort((a, b) => {
+        const valA = a[sortBy];
+        const valB = b[sortBy];
+  
+        if (typeof valA === "string" && typeof valB === "string") {
+          return sortOrder === "asc"
+            ? valA.localeCompare(valB)
+            : valB.localeCompare(valA);
+        } else {
+          return sortOrder === "asc"
+            ? parseInt(valA) - parseInt(valB)
+            : parseInt(valB) - parseInt(valA);
+        }
+      })
+    );
+  };
 
   const handleDelete = async (_id) => {
     const confirmDelete = window.confirm(texts.confirmDeleteCourse);
@@ -52,8 +70,18 @@ const Courses = () => {
           console.log(err.response.data);
           console.log(err.response.status);
           console.log(err.response.headers);
+          if (err.response.status === 401) {
+            setErrMsg(texts.forbiddenError);
+          } else if (err.response.status === 404) {
+            setErrMsg(texts.notFoundError);
+          } else if (err.response.status === 420) {
+            setErrMsg(texts.courseUsedError);
+          } else {
+            setErrMsg(texts.error);
+          }
         } else {
           console.log(`Error: ${err.message}`);
+          setErrMsg(texts.error);
         }
       }
     }
@@ -62,13 +90,34 @@ const Courses = () => {
   return (
     <section className="container-wide">
       <div className="headline">{texts.allCourses}</div>
+      <p className={errMsg ? "errmsg" : "offscreen"}>
+        {errMsg}
+      </p>
       <div>
         <table className="CourseTable">
           <thead>
             <tr>
-              <th className="td-name">{texts.id}</th>
-              <th className="td-name">{texts.name}</th>
-              <th className="td-description">{texts.description}</th>
+              <th className="td-name">
+                <button className="SortButton" onClick={() => handleSort("_id")}>
+                  {texts.id}
+                </button>
+              </th>
+              <th className="td-name">
+                <button
+                  className="SortButton"
+                  onClick={() => handleSort("title")}
+                >
+                  {texts.name}
+                </button>
+              </th>
+              <th className="td-name">
+                <button
+                  className="SortButton"
+                  onClick={() => handleSort("description")}
+                >
+                  {texts.description}
+                </button>
+              </th>
               <th className="td-actions">{texts.actions}</th>
             </tr>
           </thead>
@@ -85,14 +134,22 @@ const Courses = () => {
                 <td className="td-actions">
                   <Link to={`/courses/edit/${course._id}`}>
                     <button className="EditButton">
-                      <FontAwesomeIcon icon={faEdit} fontSize="1.4rem" style={{ color: "black" }} />
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        fontSize="1.4rem"
+                        style={{ color: "black" }}
+                      />
                     </button>
                   </Link>
                   <button
                     className="DeleteButton"
                     onClick={() => handleDelete(course._id)}
                   >
-                    <FontAwesomeIcon icon={faTrash} fontSize="1.4rem" style={{ color: "black" }} />
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      fontSize="1.4rem"
+                      style={{ color: "black" }}
+                    />
                   </button>
                 </td>
               </tr>
