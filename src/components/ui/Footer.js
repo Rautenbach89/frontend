bf23c49c8affe18d45aaff4949fb7949394bb56c
Rautenbach89 <105278662+Rautenbach89@users.noreply.github.com>
2023-03-22 07:36:21 +0100

    Update Footer.js

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <Link className="Impressum" to="/impressum">
+        <Link className="Imprint" to="/imprint">
           <p>{texts.imprintAndPrivacy}</p>
         </Link>
       </div>
